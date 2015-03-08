1c41bdac612f17baeb9301d772a070a7c7830c0a
Eugene Fang <fang.eugene@gmail.com>
2015-03-08 13:04:32 -0400

    Don't index payload

--- models/notification.py
+++ models/notification.py
@@ -2,5 +2,5 @@ from google.appengine.ext import ndb
 
 
 class Notification(ndb.Model):
-    payload = ndb.StringProperty(required=True)
+    payload = ndb.StringProperty(required=True, indexed=False)
     created = ndb.DateTimeProperty(auto_now_add=True)
